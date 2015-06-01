2b80c4f183f525dddcd38c487bd0535b035e5b6a
xottabbich@mail.ru <xottabbich@mail.ru>
2015-06-01 23:06:25 +0600

    Create new pages with content. Fix some moments.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 	$('#menu-btn').click(function(e) {
 		var menuWrap = $('#menu-wrap');
+		var pageTitle = $('.header__title-isu');
 		console.log(menuWrap);
 		
 		if (!menuWrap.hasClass("open")) {
 			menuWrap.addClass("open");
+			pageTitle.addClass("header__title-isu--open-menu");
 		} 
 		else{
 			menuWrap.removeClass("open");
+			pageTitle.removeClass("header__title-isu--open-menu");
 		};
 	});
 });
