08be4fac1ce045147e57c7355cd69150e1d6a5d7
AnnaDiner <Lutcifeer@mail.ru>
2015-06-07 19:05:19 +0600

    Fix some bugs and add href to links.

--- js/script.js
+++ js/script.js
@@ -34,4 +34,8 @@ $(document).ready(function(){
 			};
 		};
 	});
+
+	if (document.documentElement.clientHeight >= document.body.clientHeight) {
+       $('.footer').addClass("footer--fixed");
+    }
 });
