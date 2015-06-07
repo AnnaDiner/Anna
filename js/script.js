8b4a9542deba44e32d07c384d151034ce4f6ff87
AnnaDiner <Lutcifeer@mail.ru>
2015-06-07 14:27:17 +0600

    Create tablet header. Add new pages.

--- js/script.js
+++ js/script.js
@@ -13,8 +13,25 @@ $(document).ready(function(){
 			pageTitle.removeClass("header__title-isu--open-menu");
 		};
 	});
-
+	
 	$('.header-menu-item').click(function(e) {
+		if (($(window).width()+15 <=1000) && ($(window).width()+15 >= 640)) {
+			e.preventDefault();
+			var menuItem = $('.header-menu-item');
 
+			if (!$(this).hasClass("active")) {
+				for (var i = 0; i <= menuItem.length-1; i++) {
+					menuItem[i].classList.remove("active");
+				};
+				$(this).addClass("active");
+				$('.header-tablet-menu').addClass("active");
+			} 
+			else{
+				for (var i = 0; i <= menuItem.length-1; i++) {
+					menuItem[i].classList.remove("active");
+				};
+				$('.header-tablet-menu').removeClass("active");
+			};
+		};
 	});
 });
